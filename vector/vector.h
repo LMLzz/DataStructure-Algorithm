61e6afd7f0ad1befd2dff4016f9dae69fec25952
LMLzz <limingliang0527@gmail.com>
2019-08-24 15:46:16 +0800

    implement the function of "copyFrom"
    Signed-off-by: LMLzz <limingliang0527@gmail.com>

--- vector/vector.h
+++ vector/vector.h
@@ -19,17 +19,17 @@ protected:
 	int _capacity;	//设置容量
 	T* _elem;		//数据区
 	void copyFrom (T const* A, Rank lo, Rank hi);
-	void expand();	//用于容量扩充
-	void shrink();	//装填因子小时压缩
-	void bubble(Rank lo, Rank hi);
-	void bubbleSort(Rank lo, Rank hi);
-	Rank max (Rank lo, Rank hi);
-	Rank selectionSort(Rank lo, Rank hi);
-	void merge(Rank lo, Rank mi, Rank hi);
-	void mergeSort(Rank lo, Rank hi);
-	Rank partition(Rank lo, Rank hi);
-	void quickSort(Rank lo, Rank hi);
-	void heapSort(Rank lo, Rank hi);
+//	void expand();	//用于容量扩充
+//	void shrink();	//装填因子小时压缩
+//	void bubble(Rank lo, Rank hi);
+//	void bubbleSort(Rank lo, Rank hi);
+//	Rank max (Rank lo, Rank hi);
+//	Rank selectionSort(Rank lo, Rank hi);
+//	void merge(Rank lo, Rank mi, Rank hi);
+//	void mergeSort(Rank lo, Rank hi);
+//	Rank partition(Rank lo, Rank hi);
+//	void quickSort(Rank lo, Rank hi);
+//	void heapSort(Rank lo, Rank hi);
 public:
 	//构造函数
 	Vector (int c = DEFAULT_CAPACITY, int s = 0, T v = 0){ //容量为c、规模为s、所有元素初始为v
@@ -54,15 +54,16 @@ public:
 	bool empty() const {
         return !_size;
     }
-    int disordered() const;
-    Rank find(T const& e) const {
-        return find(e, 0, _size);
+}
+
+template<typename T>  //元素类型
+void Vector<T>::copyFrom(T const*A, Rank lo, Rank hi){
+    capacity = 2 * ( hi - lo );
+    _elem = new T[_capacity];
+    _size = 0;
+    while( lo < hi){
+        _elem[size++] = A[lo++];
     }
-    Rank find(T const& e, Rank lo, Rank hi) const;
-    Rank search (T const&e) const {
-        return (0>=_size) ? -1 : search(e, 0, _size);
-    }
-	//写
 }
 
 #endif
