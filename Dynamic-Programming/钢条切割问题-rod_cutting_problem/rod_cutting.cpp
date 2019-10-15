95aa4a77f484c54024c7fdbd9c6863cb07908c80
LMLzz <1660554812@qq.com>
2019-10-16 01:28:11 +0800

    print the time of execution
    Signed-off-by: LMLzz <1660554812@qq.com>

--- Dynamic-Programming/钢条切割问题-rod_cutting_problem/rod_cutting.cpp
+++ Dynamic-Programming/钢条切割问题-rod_cutting_problem/rod_cutting.cpp
@@ -8,6 +8,7 @@
 #include <iostream>
 #include <algorithm>
 #include <limits.h>
+#include <sys/time.h>
 const int p[11] = { 0,1, 5, 8, 9, 10, 17, 17, 20, 24, 30 };
 
 using std::cout;
@@ -24,9 +25,16 @@ int cut_rod(const int *p, int n){
     return q;
 }
 
+
 int main() {
+    timeval start, end;
+
     cout << "Hello World" << endl;
-    for(int i = 1; i <= 10; i++) 
+    for(int i = 1; i <= 10; i++) {
+        gettimeofday(&start, NULL);
         cout << cut_rod(p, i) << endl;
+        gettimeofday(&end, NULL);
+        cout << "Naive recusive algorithm is executed for" << 100000*(end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) << endl;
+    }
     return 0;
 }
