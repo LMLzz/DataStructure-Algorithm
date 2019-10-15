8449d41cc5e832a9b2742740a63b901045519a14
LMLzz <1660554812@qq.com>
2019-10-15 20:35:17 +0800

    naive recursive algorithm to solve rod cutting problem
    Signed-off-by: LMLzz <1660554812@qq.com>

---
+++ Dynamic-Programming/钢条切割问题-rod_cutting_problem/rod_cutting.cpp
@@ -0,0 +1,32 @@
+/*************************************************************************
+ @Author: LMLzz - limingliang0527@gmail.com
+ @Created Time : 2019-10-15 20:04:13
+ @File Name: rod_cutting.cpp
+ @Description:
+ ************************************************************************/
+
+#include <iostream>
+#include <algorithm>
+#include <limits.h>
+const int p[11] = { 0,1, 5, 8, 9, 10, 17, 17, 20, 24, 30 };
+
+using std::cout;
+using std::endl;
+using std::max;
+
+int cut_rod(const int *p, int n){
+    if (n==0) 
+        return 0;
+    int q = INT_MIN;
+    for (int i=1; i<=n; ++i) {
+        q = max(q, p[i] + cut_rod(p, n-i));
+    }
+    return q;
+}
+
+int main() {
+    cout << "Hello World" << endl;
+    for(int i = 1; i <= 10; i++) 
+        cout << cut_rod(p, i) << endl;
+    return 0;
+}
